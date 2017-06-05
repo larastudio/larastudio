30c65e7e9aa2f0d1cff3316b1a02d5206c54ee20
Jasper Frumau <jasper@imagewize.com>
2017-06-05 15:20:07 +0300

    small tweaks

--- deploy.php
+++ deploy.php
@@ -5,11 +5,14 @@ require 'recipe/laravel.php';
 
 // Configuration
 
+// Specify the repository from which to download your project's code.
+// The server needs to have git installed for this to work.
+// If you're not using a forward agent, then the server has to be able to clone
+// your project from this repository.
 set('repository', 'git@github.com:jasperf/larastudio.git');
 set('default_stage', 'production');
 set('git_tty', true); // [Optional] Allocate tty for git on first deployment
 set('ssh_type', 'native');
-set('ssh_multiplexing', true);
 add('shared_files', []);
 add('shared_dirs', []);
 add('writable_dirs', []);

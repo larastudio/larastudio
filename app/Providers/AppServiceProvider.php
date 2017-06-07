cc4a18feac2847250b4edbefbe926c33167a1041
Jasper Frumau <jasper@imagewize.com>
2017-06-07 07:37:56 +0300

    added possible future if statement for https local exclusion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        //if (!\App::environment('local')) {}
+        //https://laravel.com/docs/5.4/configuration#determining-the-current-environment
         \URL::forceScheme('https');
     }
 

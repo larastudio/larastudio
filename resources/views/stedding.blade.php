392e1287a5d6a0490984b3cc4816bd4456ae5491
Jasper Frumau <jasper@imagewize.com>
2017-06-07 09:34:13 +0300

    home and stedding basic tweaks

--- resources/views/stedding.blade.php
+++ resources/views/stedding.blade.php
@@ -1,1 +1,11 @@
-{{ $name }}
+<!DOCTYPE html>
+<html>
+<head>
+	<title>{{ $name }}</title>
+</head>
+<body>
+	<p>
+		Stedding is a minimalistic LEMP Stack setup for Laravel PHP in progress. It facilitates the setting up of Laravel apps on a well prepared server using Ansible Playbooks on a Ubuntu server. It provisions your VPS with all the tools necessary to run your Laravel PHP application with ease.
+	</p>
+</body>
+</html>

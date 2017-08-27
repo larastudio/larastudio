40353e04b0e02307e89f8f1e0590fcd53c51d966
Jasper Frumau <jasper@imagewize.com>
2017-08-27 10:13:30 +0300

    added fillable for post title and body

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+    'title',
+    'body'
+    ];
+
     public function comments ()
     {
         return $this->hasMany(Comment::class);
@@ -15,6 +20,7 @@ class Post extends Model
         // $his->comments()->create()
         // $this pseudo-variable http://php.net/manual/en/language.oop5.basic.php
         // http://stackoverflow.com/questions/1523479/what-does-the-variable-this-mean-in-php#1523484
+        // reference to the current object, it's most commonly used in object oriented code
         $this->comments()->create(compact('body'));
     }
 }

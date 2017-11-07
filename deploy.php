f9538dada641a2230fa38f181a020cbbb47cfc91
Jasper Frumau <jasper@imagewize.com>
2017-11-07 07:48:45 +0300

    .env task added, cache clearing added

--- deploy.php
+++ deploy.php
@@ -13,6 +13,7 @@ set('repository', 'git@github.com:jasperf/larastudio.git');
 set('default_stage', 'production');
 set('git_tty', true); // [Optional] Allocate tty for git on first deployment
 set('ssh_type', 'native');
+set('cachetool', '/var/run/php/php7.1-fpm.sock');
 // set('writable_mode', 'chmod');
 // set('writable_chmod_mode', '0775');
 
@@ -25,13 +26,17 @@ host('larastud.io')
     ->set('deploy_path', '/var/www/larastud.io');
 
 /**
- * Upload .env.production file as .env
+ * Upload .env.production file as .env to shared
+ * symlink from current loads .env from shared folder
  */
-// task('upload:env', function () {
-//     upload('.env.production', '{{deploy_path}}/shared/.env');
-// })->desc('Environment setup');
+
+task('upload:env', function () {
+    upload('.env.production', '/var/www/larastud.io/shared/.env');
+})->desc('Environment setup');
 
 // task('permissions:reset', function () {
 //   run('cd {{deploy_path}}');
 //   run('sudo find . -type f -exec chmod 664 {} \;');
 //   run('sudo find . -type d -exec chmod 775 {} \;');
+// Clear OPCache
+after('upload:env', 'cachetool:clear:opcache');

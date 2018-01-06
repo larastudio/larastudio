28d1f81016fd60ae80d2b533d413bd9cf8a6f430
Jasper Frumau <jasper@imagewize.com>
2018-01-06 15:52:37 +0300

    style tweak and google font load

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <!-- Custom styles for this template -->
     <link href="/css/blog.css" rel="stylesheet">
   </head>
@@ -22,7 +22,7 @@
 
     <div class="blog-header">
       <div class="container">
-        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <h1 class="blog-title">Larastudio Blog</h1>
         <p class="lead blog-description">An example blog template built with Bootstrap.</p>
       </div>
     </div>
@@ -30,7 +30,6 @@
     <div class="container">
     <div class="row">
       @yield ('content')
-      @include ('layouts.sidebar')
     </div>
     </div>
 

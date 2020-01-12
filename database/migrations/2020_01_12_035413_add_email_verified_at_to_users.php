747164c4a390d0e24c32003345d7ebdd22c14d05
Jasper Frumau <jasper@imagewize.com>
2020-01-12 15:40:50 +0700

    hasColumn check

--- database/migrations/2020_01_12_035413_add_email_verified_at_to_users.php
+++ database/migrations/2020_01_12_035413_add_email_verified_at_to_users.php
@@ -14,7 +14,9 @@ class AddEmailVerifiedAtToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('email_verified_at')->nullable();
+            if (!Schema::hasColumn('users', 'email_verified_at')) {
+                $table->timestamp('email_verified_at');
+            }
         });
     }
 

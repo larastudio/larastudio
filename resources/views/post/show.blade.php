d7fbecd8e96e06d93ff8f572ff41a860dde2f12b
Jasper Frumau <jasper@imagewize.com>
2020-01-12 09:14:52 +0700

    basic authentication + navigation changes

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel Tutorial</title>
+        <title>Larstudio</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -16,7 +16,7 @@
     <body>
 
           <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Laravel Tutorial</a>
+            <a class="navbar-brand" href="#">Larastudio Tutorial</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,34 @@ Route::get('/', function () {
 });
 
 Route::get('/blog', 'PostController@index')->name('index');
-Route::get('/blog/post/create', 'PostController@create')->name('post.create');
-Route::post('/blog/post/create', 'PostController@store')->name('post.store');
 Route::get('/blog/{slug}', 'PostController@show')->name('post.show');
-Route::get('blog/post/{id}/edit', 'PostController@edit')->name('post.edit');
-Route::post('blog/post/{id}/edit', 'PostController@update')->name('post.update');
+
+/*
+|--------------------------------------------------------------------------
+| Blog Editor Routes
+|--------------------------------------------------------------------------
+|
+| Here is where all the routes for creating, updating and storing
+| blog posts.
+|
+*/
+
+Route::get('/blog/post/create', 'PostController@create')->name('post.create')->middleware('auth');
+Route::post('/blog/post/create', 'PostController@store')->name('post.store')->middleware('auth');
+Route::get('blog/post/{id}/edit', 'PostController@edit')->name('post.edit')->middleware('auth');
+Route::post('blog/post/{id}/edit', 'PostController@update')->name('post.update')->middleware('auth');
 
 
+/*
+|--------------------------------------------------------------------------
+| Dashboard Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your Dashboard. Currently
+| we only have a basic set up here.
+|
+*/
 
+Auth::routes();
+
+Route::get('/dashboard', 'DashboardController@index')->name('dashboard');

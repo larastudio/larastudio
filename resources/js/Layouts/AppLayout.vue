7c89c55fe6066ce88d72764fc2cb3d9c78039fea
Jasper Frumau <jasper@imagewize.com>
2021-07-13 11:23:13 +0700

    desktop dropdown added

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -20,15 +20,48 @@
                                 <jet-nav-link :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </jet-nav-link>
-                                <jet-nav-link :href="route('articles')" :active="route().current('articles')">
+                                <!-- <jet-nav-link :href="route('articles')" :active="route().current('articles')">
                                     Articles
                                 </jet-nav-link>
                                 <jet-nav-link :href="route('create-articles')" :active="route().current('create-articles')">
                                     Add Article
-                                </jet-nav-link>
+                                </jet-nav-link> -->
+                                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                                    <div class="ml-3 relative">
+                                        <!-- Articles Dropdown -->
+                                        <jet-dropdown width="48">
+                                            <template #trigger>
+                                                <span class="inline-flex rounded-md">
+                                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                                        Articles
+
+                                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </template>
+
+                                            <template #content>
+                                                <!-- Account Management -->
+                                                <!-- <div class="block px-4 py-2 text-xs text-gray-400">
+                                                    Articles
+                                                </div> -->
+                                                <jet-dropdown-link :href="route('articles')">
+                                                    Articles Overview
+                                                </jet-dropdown-link>
+
+                                                <jet-dropdown-link :href="route('create-articles')">
+                                                    Add Article
+                                                </jet-dropdown-link>
+                                            </template>
+                                        </jet-dropdown>
+                                    </div>
+                                </div>
+                            
                             </div>
                         </div>
-
+                        
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
                             <div class="ml-3 relative">
                                 <!-- Teams Dropdown -->

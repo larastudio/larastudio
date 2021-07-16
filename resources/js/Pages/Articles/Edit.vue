40d379f05fd29593bff9ae01002ced6463b606d0
Jasper Frumau <jasper@imagewize.com>
2021-07-16 12:28:44 +0700

    articles crud renaming

---
+++ resources/js/Pages/Articles/Edit.vue
@@ -0,0 +1,47 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Articles
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white p-4 overflow-hidden shadow-xl sm:rounded-lg">
+                   <editor
+                    
+                    :init="{
+                        height: 500,
+                        menubar: false,
+                        plugins: [
+                        'advlist autolink lists link image charmap print preview anchor',
+                        'searchreplace visualblocks code fullscreen',
+                        'insertdatetime media table paste code help wordcount'
+                        ],
+                        toolbar:
+                        'undo redo | formatselect | bold italic backcolor | \
+                        alignleft aligncenter alignright alignjustify | \
+                        bullist numlist outdent indent | removeformat | help'
+                    }"
+                    />
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    import Editor from '@tinymce/tinymce-vue'
+
+    export default {
+        components: {
+            AppLayout,
+            'editor': Editor,
+        },
+        
+    }
+
+    
+</script>

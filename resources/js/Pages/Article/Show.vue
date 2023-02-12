51865929403dc5bfd77d2d8c283f0f942256e6ba
Jasper Frumau <jasper@imagewize.com>
2023-02-12 16:32:47 +0700

    article display update

--- resources/js/Pages/Article/Show.vue
+++ resources/js/Pages/Article/Show.vue
@@ -1,8 +1,17 @@
 <template>
   <AppLayout>
-    <Head title="Welcome" />
-    <h1>Welcome</h1>
-    <p>Hello {{ article.title }} welcome to your first Inertia app!</p>
+    <template #header>
+         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+             {{ article.title }}
+         </h2>
+         <div class="py-12">
+             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                     {{ article.content }}
+                 </div>
+             </div>
+         </div>
+     </template>
   </AppLayout>
 </template>
 
